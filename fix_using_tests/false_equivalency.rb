7ab68d1d2775f59611710aca770b8ad62bebee7c
Paul H Graham Jr <pjhg14@gmail.com>
2021-02-24 04:19:26 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,13 +12,11 @@ def prompt_user
 end
 
 def selection(num)
-  binding.pry
-  if num = 1
+  if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
-  binding.pry
 end
 
 def runner
